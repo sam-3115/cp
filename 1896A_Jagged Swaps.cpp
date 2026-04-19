9598c2789e03b63527bbaff2554d4c87c6cffdaa
samarth <123548588+sam-3115@users.noreply.github.com>
2026-04-19 19:30:46 +0530

    Add main logic for Jagged Swaps problem
    Implement solution for the Jagged Swaps problem.

---
+++ 1896A_Jagged Swaps.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int j=0;j<n;j++){
+            cin>>arr[j];
+        }
+        if(arr[0]==1){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        
+        
+    }
+}
